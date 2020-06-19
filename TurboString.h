6c569335484ef2d7c69e8615cd8b5cf1dc46dde3
Yanovich Liza <yanovich.liza@yandex.ru>
2020-06-19 10:33:12 +0300

    конструктор

--- TurboString.h
+++ TurboString.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <cstring>
+#include <ostream>
+
+class TurboString
+{
+private:
+	std::size_t m_l;
+	char m_s[256];
+public:
+	TurboString(const char* s = nullptr)
+	{
+		m_l = 0;
+		m_s[0] = 0;
+		if (!s) {
+			return;
+		}
+		std::size_t l = strlen(s);
+		if (l > 255) {
+			throw std::overflow_error("TurboString::TurboString");
+		}
+		m_l = l;
+		memcpy(m_s, s, l + 1);
+	}
+
+	friend std::ostream& operator <<(std::ostream& out, const TurboString& s);
+};
--- dz3.cpp
+++ dz3.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include "TurboString.h"
+
+using namespace std;
 
 int main()
 {
+	TurboString s("TurboString");
+	cout << s << endl;
 	return 0;
 }
